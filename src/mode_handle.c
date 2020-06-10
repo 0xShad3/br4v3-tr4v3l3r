c0246d87bdbcfcbba1582422d6f7fbd05238562e
restylianos <restemstelios@yahoo.com>
2020-06-11 00:16:57 +0300

    Bug fix
    Check previous version for the mistake of the double call to function

--- src/mode_handle.c
+++ src/mode_handle.c
@@ -405,7 +405,7 @@ void *multi_recv_handler(void *args)
             /**
               * Player Functions1
               */
-
+            printf("Repsone :id %s",response_id);
             itoa(PLR_DEATH_ID_P, comp, 10);
             if (!strcmp(response_id, comp))
             {
@@ -416,26 +416,17 @@ void *multi_recv_handler(void *args)
             }
 
             itoa(PLR_MOVE_ID_P, comp, 10);
-            if (!strcmp(response_id, comp))
+            if (strcmp(response_id, comp)==0)
             {
                 if (decode_on_player_move(game->players, net_buffer) != 0)
                 {
-                    // send message to server for message loss
+                   printf("WEASDASDASDASD\n\n");
                 }
             }
 
             itoa(PLR_UPDATE_ID_P, comp, 10);
-            if (!strcmp(response_id, comp))
-            {
-                if (decode_on_player_update_stats(game->players, net_buffer) != 0)
+                if (!strcmp(response_id, comp))
                 {
-                    // send message to server for message loss
-                }
-            }
-
-            itoa(PLR_UPDATE_ID_P, comp, 10);
-            if (!strcmp(response_id, comp))
-            {
                 if (decode_on_player_update_stats(game->players, net_buffer) != 0)
                 {
                     // send message to server for message loss
